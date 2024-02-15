b47fdef9c9309353b466691f0045ac437472ef38
amitkasabe <amit.kasabe25@gmail.com>
2024-02-16 00:28:29 +0530

    added profile

--- app/Views/admin/static/components/topnav.php
+++ app/Views/admin/static/components/topnav.php
@@ -143,7 +143,7 @@
 		  </a>
 
 						<a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown">
-			<img src="img/avatars/avatar.jpg" class="avatar img-fluid rounded me-1" alt="admin" /> <span class="text-dark"><?php echo "admin" ?></span>
+			<img src="https://scontent.fnag1-5.fna.fbcdn.net/v/t1.6435-9/116710652_149681633426432_871182394667912108_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=be3454&_nc_ohc=L0_lyISGMAQAX8hgiwv&_nc_ht=scontent.fnag1-5.fna&oh=00_AfCWonj4GMFsadHYILzO3tr9LU6azHfiogtC0jL81CFV1A&oe=65F5C948" class="avatar img-fluid rounded me-1" alt="admin" /> <span class="text-dark"><?php echo "admin" ?></span>
 		  </a>
 						<div class="dropdown-menu dropdown-menu-end">
 							<a class="dropdown-item" href="pages-profile.html"><i class="align-middle me-1" data-feather="user"></i> Profile</a>
