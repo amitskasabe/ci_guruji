717697743c5c08f8400bbfbb1f8c001572e25786
amitkasabe <amit.kasabe25@gmail.com>
2024-02-18 14:59:33 +0530

    removed message and notifications

--- app/Views/admin/static/components/header.php
+++ app/Views/admin/static/components/header.php
@@ -32,12 +32,7 @@
       <ul class="sidebar-nav">
         <li class="sidebar-header">Pages</li>
 
-        <li class="sidebar-item active">
-          <a class="sidebar-link" href="dashboard">
-            <i class="align-middle" data-feather="sliders"></i>
-            <span class="align-middle">Dashboard</span>
-          </a>
-        </li>
+      
 
         <!-- <li class="sidebar-item">
           <a class="sidebar-link" href="pages-profile.html">
